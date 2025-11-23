4ec83c8c0b2c90320a1c38321a3d9e24319a8cb6
ThisIsSumit <sumitkumar453827@gmail.com>
2025-11-23 23:18:57 +0530

    updated

---
+++ src/test/java/com/socialsync/socialsyncbackend/SocialSyncBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialsync.socialsyncbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialSyncBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
