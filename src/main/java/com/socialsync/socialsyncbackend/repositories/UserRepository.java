77f991958e93c9dc3df3ef31de5d91429094879f
ThisIsSumit <sumitkumar453827@gmail.com>
2025-11-27 17:51:40 +0530

    adding security

--- src/main/java/com/socialsync/socialsyncbackend/repositories/UserRepository.java
+++ src/main/java/com/socialsync/socialsyncbackend/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package com.socialsync.socialsyncbackend.repositories;
 
-import org.apache.catalina.User;
+
+import com.socialsync.socialsyncbackend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
